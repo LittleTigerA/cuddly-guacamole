3563fda4d7f5d448828f1cf7c0aa507f74d51a3e
little-tiger <littletiger2333@gmail.com>
2023-01-09 12:19:41 +0800

    It works!

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
 
 namespace Culldy
 {
@@ -10,6 +8,53 @@ namespace Culldy
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No Args");
+                Environment.Exit(1);
+            }
+            switch (args[0])
+            {
+                case "launch":
+                    LaunchGame();
+                    break;
+                default:
+                    Console.WriteLine("Unknown Args");
+                    Console.WriteLine(args[0]);
+                    break;
+            }
+        }
+        static void LaunchGame()
+        {
+            string winver = (string)Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue("ProductName");
+            if (winver.IndexOf("Windows 10") > -1 || winver.IndexOf("Windows 11") > -1)
+            {
+                using (Process p = new Process())
+                {
+                    p.StartInfo.FileName = "cmd.exe";
+                    p.StartInfo.UseShellExecute = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.RedirectStandardError = true;
+
+                    p.Start();
+
+                    p.StandardInput.WriteLine("powershell /c start \"shell:appsfolder\\$(Get-AppxPackage -Name Microsoft.MinecraftUWP | select -ExpandProperty packagefamilyname)!app\" && exit");
+                    p.StandardInput.AutoFlush = true;
+                    if (!(p.StandardError.ReadLine() == null))
+                    {
+                        Console.WriteLine("Start Failed");
+                        Environment.Exit(1);
+                    }
+
+                    p.WaitForExit();
+                    p.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not Support System");
+                Environment.Exit(1);
+            }
         }
     }
 }
